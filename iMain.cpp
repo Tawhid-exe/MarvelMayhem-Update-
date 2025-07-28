96c2da65eec046bb0a48eab9d3dd6feef300a19f
Maruf Hasan Tawhid <tawhidit3@gmail.com>
2025-07-28 11:36:15 +0600

    Blink Animation Merged, loading function separated from Keyboard

--- iMain.cpp
+++ iMain.cpp
@@ -3,21 +3,22 @@
 
 int loadingScreen;
 
+// Loading screen variables
 int loadingBarWidth = 0;
 bool loadingDone = false;
 bool goToMainMenu = false;
-
 bool blinkTextWhite = true;
 
 void iDraw()
 {
 	iClear();
 
-	if (!goToMainMenu){
-		// Show loading screen background
+	if (!goToMainMenu)
+	{
+		// Show loading screen
 		iShowImage(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, loadingScreen);
 
-		// Draw loading bar fill (white)
+		// Loading bar frame and fill
 		iSetColor(255, 255, 255);
 		iRectangle(390, 100, 500, 30);
 		iFilledRectangle(390, 100, loadingBarWidth, 30);
@@ -37,65 +38,91 @@ void iDraw()
 			iText(390, 140, "Loading...", GLUT_BITMAP_HELVETICA_18);
 		}
 	}
-	else{
-		// Show main menu screen background after loading completes and user presses SPACE
-		hoverAnimation();
+	else
+	{
+		// Show the actual menu screen after loading
+		showMenu();
 	}
 }
+
 
 void iMouseMove(int mx, int my)
 {
 	
 }
 
+
 void iPassiveMouseMove(int mx, int my)
 {
+	
+	if (goToMainMenu) {
+		// By default, assume nothing is hovered
+		hoveredButtonIndex = -1;
+		hoveredOptionButtonIndex = -1;
+		backButtonHover = false;
 
-	hover(mx, my);
+		if (currentScreen == -1) { // On the main menu
+			for (int i = 0; i < BUTTON_COUNT; i++) {
+				if (mx >= buttons[i].x && mx <= buttons[i].x + buttons[i].width &&
+					my >= buttons[i].y && my <= buttons[i].y + buttons[i].height) {
+					hoveredButtonIndex = i;
+					break;
+				}
+			}
+		}
+		else if (currentScreen == 0) { // On the Play Sub-Menu
+			// hover for the two option buttons
+			for (int i = 0; i < 2; i++) {
+				if (mx >= optionButtons[i].x && mx <= optionButtons[i].x + optionButtons[i].width &&
+					my >= optionButtons[i].y && my <= optionButtons[i].y + optionButtons[i].height) {
+					hoveredOptionButtonIndex = i;
+					return; // Exit after finding a hover
+				}
+			}
+			//hover for the back button
+			if (mx >= backButton.x && mx <= backButton.x + backButton.width &&
+				my >= backButton.y && my <= backButton.y + backButton.height) {
+				backButtonHover = true;
+			}
+		}
+		else { // On any other sub-screen (Settings, About, Option 1, Option 2)
+			// back button hover
+			if (mx >= backButton.x && mx <= backButton.x + backButton.width &&
+				my >= backButton.y && my <= backButton.y + backButton.height) {
+				backButtonHover = true;
+			}
+		}
+	}
 }
+
 
 void iMouse(int button, int state, int mx, int my)
 {
-
-	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+	if (goToMainMenu)
 	{
-		if (goToMainMenu){
-			handleMenuClick(mx, my);
-		}
-	}
-
-
-	if (button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN)
-	{
-
+		// Handle menu clicks only after the loading screen
+		handleMenuClick(button, state, mx, my);
 	}
 }
 
+// This function runs at a fixed interval
 // Special Keys:
 // GLUT_KEY_F1, GLUT_KEY_F2, GLUT_KEY_F3, GLUT_KEY_F4, GLUT_KEY_F5, GLUT_KEY_F6, GLUT_KEY_F7, GLUT_KEY_F8, GLUT_KEY_F9, GLUT_KEY_F10, GLUT_KEY_F11, GLUT_KEY_F12, 
 // GLUT_KEY_LEFT, GLUT_KEY_UP, GLUT_KEY_RIGHT, GLUT_KEY_DOWN, GLUT_KEY_PAGE UP, GLUT_KEY_PAGE DOWN, GLUT_KEY_HOME, GLUT_KEY_END, GLUT_KEY_INSERT
 
 void fixedUpdate()
 {
-	if (isKeyPressed('w') || isSpecialKeyPressed(GLUT_KEY_UP))
-	{
+	// Game logic can go here for player movement etc.
+	if (isKeyPressed('w') || isSpecialKeyPressed(GLUT_KEY_UP)) {}
+	if (isKeyPressed('a') || isSpecialKeyPressed(GLUT_KEY_LEFT)) {}
+	if (isKeyPressed('s') || isSpecialKeyPressed(GLUT_KEY_DOWN)) {}
+	if (isKeyPressed('d') || isSpecialKeyPressed(GLUT_KEY_RIGHT)) {}
 
-	}
-	if (isKeyPressed('a') || isSpecialKeyPressed(GLUT_KEY_LEFT))
-	{
-
-	}
-	if (isKeyPressed('s') || isSpecialKeyPressed(GLUT_KEY_DOWN))
-	{
-
-	}
-	if (isKeyPressed('d') || isSpecialKeyPressed(GLUT_KEY_RIGHT))
-	{
-
-	}
+	
 
 }
 
+// Handles the loading bar animation
 void loadingBar(){
 	if (!loadingDone){
 
@@ -109,6 +136,11 @@ void loadingBar(){
 	else{
 		if (isKeyPressed(' ')){
 			goToMainMenu = true;
+			// Load menu 
+			if (mainMenuScreen == -1)
+			{
+				loadMenuAssets();
+			}
 		}
 	}
 }
@@ -117,20 +149,19 @@ void toggleBlinkColor() {
 	blinkTextWhite = !blinkTextWhite;
 }
 
-
 int main()
 {
+	// Initialize graphics window
 	iInitialize(SCREEN_WIDTH, SCREEN_HEIGHT, "Marvel Mayhem");
 
+	//loading screen background image
 	loadingScreen = iLoadImage("BG/loading.png");
-
-	loadMenuAssets();
 
 	iSetTimer(350, toggleBlinkColor); // 350 ms = 0.35 sec toggle
 
 	iSetTimer(10, loadingBar);
 
-	iStart();
+	iStart(); // Start the graphics engine
 
 	return 0;
 }
